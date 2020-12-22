9dd17d7fb5a70c9d23ac6490ed1f8e0dae71e26d
AryPat <69769265+AryPat@users.noreply.github.com>
2020-12-22 10:42:28 -0500

    Started on About

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header";
+import About from "./Components/About";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <div class="whyThis">
+        <About></About>
+      </div>
     </div>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../App.css";
+import "../Styles/About.css";
+
+class About extends Component {
+  render() {
+    return (
+      <div class="center">
+        <div class="about"></div>
+      </div>
+    );
+  }
+}
+
+export default About;
