cdecc95aa890a76568bb62241ca2a7fa997d6ab2
Aryan Patel <69769265+AryPat@users.noreply.github.com>
2020-12-23 07:37:41 -0500

    Finished About

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { FaGithub, FaLinkedin,   } from 'react-icons/fa';
 import "../App.css";
 import "../Styles/About.css";
 
@@ -6,7 +7,16 @@ class About extends Component {
   render() {
     return (
       <div class="center">
-        <div class="about"></div>
+        <div class="about center">
+          <p class="text top"> Thrid Year student @ <strong style={{color: '#e94560'}}> University of Toronto</strong>. Specializing in <strong style={{color: '#e94560'}}>Computer Science</strong> Software Engineering Stream. </p>
+          <strong class="text">Feel free to get in contact via</strong>
+          <div class="linked">
+          <FaGithub />
+          <FaLinkedin />
+
+        </div>
+        </div>
+        
       </div>
     );
   }
