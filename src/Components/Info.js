957247c2135fd6defaf94137b25fcdaf671ee85a
Aryan Patel <69769265+AryPat@users.noreply.github.com>
2021-01-01 16:18:36 -0500

    Hotfix

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -312,7 +312,7 @@ class nav extends Component {
                   <a href="https://github.com/AryPat/portfolio" target="__blank">
                     <img src={github} alt="logo" width="40rem" class="translateLogo" />
                   </a>
-                  <a href="https://plznohate.herokuapp.com/" target="__blank">
+                  <a href="https://plznohate.herokuapp.com/">
                     <img src={goto} alt="logo" width="35rem" class="translateLogo" />
                   </a>
                 </div>
